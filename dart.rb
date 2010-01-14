dc88c5ef8c75243efb9b09d6ec94fd078c250f90
Ross Paffett <ross@rosspaffett.com>
2010-01-13 19:45:03 -0500

    Dart "pure" now uses all Ruby code for its actions

--- dart.rb
+++ dart.rb
@@ -1,21 +1,16 @@
 #!/usr/bin/env ruby
-
 %w(rubygems summer).each { |lib| require lib }
 
 module Dart
-  module Actions; end
-  
   class Bot < Summer::Connection
-    include Dart::Actions
-    
     def channel_message(sender, channel, message)
       handle_message(sender, channel, message)
     end
-  
+
     def private_message(sender, bot, message)
       handle_message(sender, sender[:nick], message)
     end
-  
+
     def handle_message(sender, source, message)
       Thread.new(sender, source, message) do
         if @config[:owner]
@@ -25,29 +20,13 @@ module Dart
         if message =~ /^\s*#{me}\s*[:,]\s*(.*)$/i
           args = $1.split
           command = args.shift
-        else
-          return
+          
+          eval @config[:commands][command]['do'] if @config[:commands].key?(command)
         end
-
-        @config[:commands].each_pair do |name, config|
-          next unless name.casecmp(command) == 0
-
-          config = HashWithIndifferentAccess.new(config)
-
-          config[:actions].each do |action|
-            action = HashWithIndifferentAccess.new(action)
-            action.each_pair { |key, val| action[key] = val.dartify(sender, args) unless key == 'type' }
-            type = (action[:type] + '_action').to_sym
-            
-            send(type, action, sender, source, args) if respond_to?(type)
-          end
-        end
-      end # Thread.new
-    end
+      end
+    end # handle_message
   end
 end
-
-Dir.glob('lib/**/*.rb') { |lib| require lib }
 
 config = HashWithIndifferentAccess.new(YAML::load_file(File.dirname($0) + '/config/summer.yml'))
 Dart::Bot.new(config[:server])
